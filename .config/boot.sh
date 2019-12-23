9b367d27166b94a156c28e3bf684efba4e136c4f
Jesse Kelly <kellyjesse881@gmail.com>
2019-12-23 10:18:07 -0600

    removed delay from boot

--- .config/boot.sh
+++ .config/boot.sh
@@ -15,7 +15,8 @@ dunst -lb "$color1" -nb "$color2" -hb "$color3" -lf "#000000" -mf "#000000" -bf 
 
 # Post setup launch programs
 urxvt -e tmux attach &
-sleep 1s
 i3-msg "workspace 2"
 google-chrome-stable &
 conky &
+
+remind -z1 -k'notify-send %s &' $HOME/docs/personal/reminders # reminders
