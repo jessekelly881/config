1cf8475e642982e5177909ef4592ffadfb691502
Jesse Kelly <kellyjesse881@gmail.com>
2020-01-06 12:52:07 -0600

    added both conky configs to boot

--- .config/boot.sh
+++ .config/boot.sh
@@ -17,7 +17,8 @@ dunst -lb "$color1" -nb "$color2" -hb "$color3" -lf "#000000" -mf "#000000" -bf 
 urxvt -e tmux attach &
 i3-msg "workspace 2"
 google-chrome-stable &
-conky &
+conky -c $HOME/.config/conky/system.conf &
+conky -c $HOME/.config/conky/personal.conf &
 
 remind -z1 -k'notify-send %s &' $HOME/docs/personal/reminders & # reminders
 xss-lock -- i3lock-fancy &
