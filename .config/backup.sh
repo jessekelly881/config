8b06cd8d5b3c693b0079bace4f2bbc251326d146
Jesse Kelly <kellyjesse881@gmail.com>
2020-01-23 16:01:05 -0600

    backup: removed notifications

--- .config/backup.sh
+++ .config/backup.sh
@@ -2,11 +2,8 @@
 # 0 */2 * * * . ~/.config/backup.sh
 
 set -x
-notify-send "Backup" "Backup started"
 
 # music
 MUSIC_BUCKET="s3://jesse-music/"
 MUSIC_FOLDER="$HOME/media/music"
 s3cmd sync -r $MUSIC_FOLDER $MUSIC_BUCKET >>  ~/.backup-log
-
-notify-send "Backup" "Backup completed"
