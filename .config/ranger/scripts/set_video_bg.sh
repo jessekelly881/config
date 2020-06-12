e2d48118caddcaea3f5f0c55e57db2b5c0d87f81
Jesse Kelly <kellyjesse881@gmail.com>
2020-06-12 17:01:02 -0500

    ranger: added scripts

---
+++ .config/ranger/scripts/set_video_bg.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+FILE="$@"
+IMG_FILE="/tmp/bgvi.jpeg"
+
+notify-send "Video BG" $FILE
+
+yes | ffmpeg \
+  -ss 00:01:00 \
+  -i $FILE \
+  -vframes 1 \
+  $IMG_FILE
+
+
+wal -i $IMG_FILE
+xwinwrap -ov -fs -- mpv -vo=gpu -wid WID -no-audio -no-osd-bar -player-operation-mode=cplayer --hwdec=vaapi --no-osc --ab-loop-a=0:30 --ab-loop-b=3:00 --start=0:30 --no-correct-pts $FILE
